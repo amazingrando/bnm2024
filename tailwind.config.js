5039858da0441518acc74e850dd07f2d981b8c0a
Randy Oest <oest@amazingrando.com>
2024-05-16 08:46:43 -0400

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const colorTokens = require('./dist/tokens/colors.tailwind.js');
+// const fontSizeTokens = require('./src/tokens/typography.json');
+
+const createTailwindPreset = (tokens) => ({
+  theme: {
+    extend: {
+      colors: {
+        ...tokens.color,
+      },
+    },
+  },
+});
+
+const preset = createTailwindPreset(colorTokens);
+
 export default {
   content: ["./src/**/*.{html,js,twig}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colorTokens,
+      },
+    },
   },
   plugins: [],
 }
