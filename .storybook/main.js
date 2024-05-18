e668f8c56aa42a2dc69daa4004ad9e2770d6c9b4
Randy Oest <oest@amazingrando.com>
2024-05-18 12:55:01 -0400

    emulsify init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,21 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/components/**/*.mdx",
-  "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+module.exports = {
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+  core: {
+    builder: 'webpack5',
+  },
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/html-webpack5',
     options: {},
   },
-  previewBody: (body) => `
-    <script>
-    window.Drupal = window.Drupal || {behaviors: {}};
-    window.drupalSettings = Object.assign(window.drupalSettings || {}, {
-      // Mock any drupalSettings your behaviors need here.
-    });
-    // Mock Drupal's once library too.
-    window.once = (_, selector) => document.querySelectorAll(selector);
-    document.addEventListener('DOMContentLoaded', () => {
-      Object.entries(window.Drupal.behaviors).forEach(([key, object]) => object.attach(document));
-    })
-  </script>
-  ${body}
-  `
+  docs: {
+    autodocs: true,
+  },
 };
-export default config;
--- src/components/header/header.js
+++
@@ -1,8 +0,0 @@
-Drupal.behaviors.accordion = {
-  attach(context) {
-    console.log('header fired');
-    const openMenu = context.getElementById('small-screen-menu');
-    console.log('openMenu', openMenu);
-    openMenu.addEventListener('click', console.log('openMe'));
-  },
-};
