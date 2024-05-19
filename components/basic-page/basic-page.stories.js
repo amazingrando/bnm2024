39e788647a0cbd37d50f2d5bf28cef10748be065
Randy Oest <oest@amazingrando.com>
2024-05-19 11:57:35 -0400

    chore: wip

--- components/basic-page/basic-page.stories.js
+++ components/basic-page/basic-page.stories.js
@@ -1,10 +1,11 @@
 import template from './basic-page.twig';
+import data from './basic-page.yml';
 import '../header/header';
 
 const component = {
   title: 'Pages/Basic Page',
   render: (args) => template(args),
-  args: {},
+  args: { ...data },
 };
 
 export const TitleElement = {
